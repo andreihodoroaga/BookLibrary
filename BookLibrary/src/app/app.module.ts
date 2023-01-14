590776cc87669d31712aeb9969ef92ac6406a1cc
Andrei Hodoroaga <andreihodoroaga36@gmail.com>
2023-01-14 22:03:02 +0200

    intialised angular project, basic setup, routing

--- BookLibrary/src/app/app.module.ts
+++ BookLibrary/src/app/app.module.ts
@@ -3,13 +3,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './core/components/footer/footer.component';
+import { HeaderComponent } from './core/components/header/header.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BooksComponent } from './pages/books/books.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    BooksComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule
+    BrowserModule, HttpClientModule, AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
