b129ca04fa508c4dcf96e7e2137c851d430fc01c
Andrei Hodoroaga <andreihodoroaga36@gmail.com>
2023-01-19 16:35:49 +0200

    form for adding books done

--- BookLibrary/src/app/core/services/books.service.ts
+++ BookLibrary/src/app/core/services/books.service.ts
@@ -1,32 +1,53 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Book } from '../components/book/book.model';
-import { map } from 'rxjs';
+import { Observable, catchError, map, throwError } from 'rxjs';
 import { genres } from 'src/app/shared/models/genre.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BooksService {
+  apiUrl = '/api/books';
 
   constructor(private httpClient: HttpClient) {
 
   }
 
   getAllBooks() {
-    return this.httpClient.get<Book[]>('/api/books').pipe(
+    return this.httpClient.get<Book[]>(this.apiUrl).pipe(
       map(booksData => booksData.map(
         book => ({
           title: book.title,
-          author: book.author ? book.author : "Autor necunoscut",
           description: book.description,
           genre: book.genre,
           rating: book.rating,
           pageCount: book.pageCount,
-          publishedDate: book.publishedDate.split("T")[0],
+          publishedDate: book.publishedDate,
           authorName: book.authorName
         })
       ))
     );
+  }
+
+  addBook(book: Book): Observable<Book> {
+    return this.httpClient.post<Book>(this.apiUrl + '/add', book)
+      .pipe(
+        catchError(this.handleError)
+      );
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+      console.error(
+        `Backend returned code ${error.status}, body was: `, error.error);
+    }
+    // Return an observable with a user-facing error message.
+    return throwError(() => new Error('Something bad happened; please try again later.'));
   }
 }
--- BookLibrary/src/app/pages/books/add-book/add-book.component.ts
+++ BookLibrary/src/app/pages/books/add-book/add-book.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { BooksService } from 'src/app/core/services/books.service';
 import { genres } from 'src/app/shared/models/genre.model';
+import { Book } from 'src/app/core/components/book/book.model';
 
 @Component({
   selector: 'app-add-book',
@@ -16,24 +18,42 @@ export class AddBookComponent implements OnInit {
   addBookForm = new FormGroup({
     title: new FormControl(''),
     description: new FormControl(''),
-    author: new FormControl(''),
+    authorName: new FormControl(''),
     pageCount: new FormControl(0, [Validators.required, Validators.pattern(/^\d+$/)]),
-    genre: new FormControl(''),
+    genre: new FormControl(),
     rating: new FormControl(0, [Validators.required, Validators.pattern(/^(?:[1-4](?:\.[0-9]{1,2})?|5(?:\.0{1,2})?)$/)]),
     publishedDate: new FormControl<Date | null>(null),
-    authorId: new FormControl('')
   })
 
-  constructor() { }
+  constructor(private readonly booksService: BooksService) { }
 
   ngOnInit(): void {
+  }
+
+  addBook(book: Book) {
+    this.booksService.addBook(book).subscribe(response => {
+      console.log(response)
+    })
   }
 
   onSubmit() {
     if (this.addBookForm.invalid)
       return;
-
-    console.log(this.addBookForm.value.title)
+    
+    const newBook: Book = {
+      title: this.addBookForm.value.title!,
+      description: this.addBookForm.value.description!,
+      pageCount: this.addBookForm.value.pageCount!,
+      rating: this.addBookForm.value.rating!,
+      genre: this.getGenre(this.addBookForm.value.genre!),
+      publishedDate: this.addBookForm.value.publishedDate!,
+      authorName: this.addBookForm.value.authorName!,
+    }
+    
+    this.addBook(newBook);
   }
 
+  getGenre(genreName: string) {
+    return genres.indexOf(genreName);
+  }
 }
